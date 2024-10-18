42da980b1746f059eaaa5274114f4d8cbef76524
Neel Gorasiya <neel@surfmobil.no>
2024-10-18 22:30:06 +0530

    added few validations

--- index.js
+++ index.js
@@ -2,47 +2,47 @@ import { SMTPServer } from "smtp-server";
 import { simpleParser } from "mailparser";
 import { saveNewMail } from "./controllers/mail.js";
 import mongoose from "mongoose";
+import "dotenv/config";
 
-const MONGO_URI = "mongodb+srv://Neel:neel2904@mail-room.m24vx.mongodb.net/";
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(console.log("MongoDB connected!!"))
+  .catch((error) => console.error({ error }));
 
-try {
-  const server = new SMTPServer({
-    allowInsecureAuth: true,
-    authOptional: true,
-    onConnect(session, cb) {
-      console.log("onConnect", session.id);
-      cb(); //Accept connection
-    },
+const server = new SMTPServer({
+  allowInsecureAuth: true,
+  authOptional: true,
+  onConnect(session, callback) {
+    if (session.remoteAddress === "127.0.0.1") {
+      return callback(new Error("No connections from localhost allowed"));
+    }
+    return callback();
+  },
 
-    onMailFrom(address, session, cb) {
-      console.log("onMailFrom", address.address, session.id);
-      cb();
-    },
+  onMailFrom(address, session, callback) {
+    console.log("onMailFrom", address.address, session.id);
+    callback();
+  },
 
-    onRcptTo(address, session, cb) {
-      console.log("onRcptTo", address.address, session.id);
-      cb();
-    },
+  onRcptTo(address, session, callback) {
+    if (address.address !== "neel@mail-room.site") {
+      return callback(
+        new Error("Only neel@mail-room.site is allowed to receive mail")
+      );
+    }
+    callback();
+  },
 
-    onData(stream, session, cb) {
-      stream.on("data", (data) => console.log(`onData ${data.toString()}`));
-      simpleParser(stream, {}, async (err, parsed) => {
-        const savedMail = await saveNewMail(parsed);
-        console.log("Mail saved successfully!", { savedMail });
-      });
-      stream.on("end", cb);
-    },
-  });
-  mongoose.connect(MONGO_URI).then(console.log("MongoDB connected!!"));
-  const savedMail = await saveNewMail({
-    from: "Neel",
-    to: "Neel",
-    subject: "Anything",
-    body: "Test",
-    attachments: [],
-  });
-  console.log({ savedMail });
-  server.listen(25, () => console.log("Server running at port: 25"));
-} catch (error) {
-  throw new Error(error?.message);
-}
+  onData(stream, session, callback) {
+    stream.on("data", (data) => console.log(`onData ${data.toString()}`));
+    simpleParser(stream, {}, async (err, parsed) => {
+      const savedMail = await saveNewMail(parsed);
+      console.log("Mail saved successfully!", { savedMail });
+    });
+    stream.on("end", callback);
+  },
+
+  onClose(session) {},
+});
+server.listen(25, () => console.log("Server running at port: 25"));
+server.on("error", (error) => console.log(error.message));
